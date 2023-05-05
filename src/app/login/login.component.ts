6a815fe5712118c7ea9bff29420d242aa0daa867
pooja <poojayadav@gmail.com>
2023-05-05 16:13:02 +0530

    yes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  {
-  
+  constructor(private service:UserService)
+  {
+
+  }
   public  itemdata= [
     {"img": "assets/assets/images/meeting-04.jpg" , "para": "Some quick example text to build on the card title and make up the bulk of the card's content."},
     {"img": "assets/assets/images/meeting-04.jpg"  , "para": "Some quick example text to build on the card title and make up the bulk of the card's content."},
@@ -15,5 +19,17 @@ export class LoginComponent  {
     
     
   ]
+  title = 'forms';
+    process(data:any)
+    {
+      console.log(data);
+      
+      this.service.login(data).subscribe((result)=>{
+        console.log(result);
+      });
+    }
+  
 
 }
+
+
